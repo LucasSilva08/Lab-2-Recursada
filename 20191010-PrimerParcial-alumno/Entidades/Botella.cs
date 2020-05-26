3e8d64ccba42efe44f51fbac0f50885b795b9e93
fierrito2015 <lucasnicolas.silva@hotmail.com.ar>
2020-05-26 17:10:04 -0300

    Generico

---
+++ 20191010-PrimerParcial-alumno/Entidades/Botella.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Botella
+    {
+        public enum Tipo
+        {
+            Vidrio,
+            Plastico
+        }
+        #region Atributos
+        protected int capacidadML;
+        protected int contenidoML;
+        protected string marca;
+        #endregion
+
+        #region Constructor
+        public Botella(string marca,int capacidadML,int contenidoML)
+        {
+            if(capacidadML<contenidoML)
+            {
+                this.contenidoML = capacidadML;
+                this.capacidadML = capacidadML;
+            }
+            else
+            {
+                this.capacidadML = capacidadML;
+                this.contenidoML = contenidoML;
+            }
+            this.marca = marca;
+        }
+        #endregion
+
+        #region Propiedades
+        public int CapacidadLitros { get {return (this.capacidadML/1000); } }
+        public int Contenido { get {return this.contenidoML; } set {this.contenidoML=value; } }
+        public float PorcentajeContenido
+        {
+            get
+            {
+              return ((float)this.contenidoML*100)/this.capacidadML;
+            }
+        }
+        #endregion
+
+        #region Metodos
+        public abstract int ServirMedida();
+
+        protected virtual string GenerarInforme()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("CAPACIDAD: {0}ml\n", this.capacidadML);
+            sb.AppendFormat("CONTENIDO: {0}ml\n", this.contenidoML);
+            sb.AppendFormat("MARCA: {0}\n", this.marca);
+            return sb.ToString();
+        }
+        #endregion
+
+        #region SobreCargas
+        public override string ToString()
+        {
+            return this.GenerarInforme();
+        }                 
+        
+        #endregion
+    }
+}
---
+++ 20191010-PrimerParcial-alumno/Entidades/Cerveza.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cerveza:Botella
+    {
+        #region
+        private const int MEDIDA=330;
+        private Tipo tipo;
+        #endregion
+
+        #region Constructor
+        public Cerveza(int capacidadML,string marca,int contenidoML):base(marca,capacidadML,contenidoML)
+        {
+            this.tipo = Tipo.Vidrio;
+        }
+        public Cerveza(int capacidadML,string marca,Tipo tipo,int contenidoML):this(capacidadML,marca,contenidoML)
+        {
+            this.tipo = tipo;
+        }
+        #endregion
+
+        #region Metodos
+        public override int ServirMedida()
+        {
+            if(MEDIDA<=this.contenidoML)
+            {
+                this.Contenido = this.contenidoML - ((80 * MEDIDA) / 100);
+            }
+        }
+        #endregion
+    }
+}
